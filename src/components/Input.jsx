e9464ff9daf0df8f6c810c9cd41a4f0320dec2c1
JunioDuarte <junioduarte31@gmail.com>
2024-10-01 13:16:27 -0300

    commit inicial

---
+++ src/components/Input.jsx
@@ -0,0 +1,9 @@
+function Input(props) {
+  return (
+    <input
+      className="border px-4 py-2 bg-slate-300 outline-slate-400 rounded-md"
+      {...props}
+    />
+  );
+}
+export default Input;
